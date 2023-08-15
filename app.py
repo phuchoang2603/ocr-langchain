1fb138816b6f5cfbef829af2699a5e6b1d4f58b7
timz06 <91061595+phuchoang2603@users.noreply.github.com>
2023-08-15 22:39:09 +0700

    modify for flask api

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from ocr import return_product
+from os import mkdir
+import os.path as path
+
+if(not(path.exists('./output_imgs'))):
+    mkdir('./output_imgs')
+
+app = Flask(__name__)
+
+@app.route('/ocr', methods=['POST'])
+def ocr():
+    img = request.files['img']
+    img_path = './output_imgs/img.jpg'
+    img.save(img_path)
+    product = return_product(img_path)
---
+++ ocr.py
@@ -0,0 +1,57 @@
+from pyzbar import pyzbar
+import cv2
+import requests
+from lxml import html
+import re
+
+def read_barcode (frame):
+    barcodes = pyzbar.decode(frame)
+    barcode_info = None
+
+    for obj in barcodes:
+        barcode_info = (obj.data.decode("utf-8"))
+
+    if len(barcode_info) > 0:
+        return barcode_info
+    else:
+        return "Please scan again"
+
+def lookup_barcode (barcode):
+    url = "https://icheck.vn/san-pham/" + barcode
+
+    try:
+        response = requests.get(url)
+        tree = html.fromstring(response.content)
+        title = tree.xpath('//title/text()')[0]
+        description = tree.xpath('//meta[@name="description"]/@content')[0]
+
+        price_pattern = r"Giá: (.*?) đ"
+        price = re.search(price_pattern, description).group(1).replace(",", "")
+
+        publisher_pattern = r"của (.*?) iCheck.vn"
+        publisher = re.search(publisher_pattern, description).group(1)
+
+        return title, price, publisher
+    except:
+        return "Not found the product"
+
+def return_product(path):
+    # get the image
+    image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+    # # read the barcode
+    number = read_barcode(image)
+
+    # lookup the barcode
+    title = lookup_barcode(number)[0]
+    price = lookup_barcode(number)[1]
+    publisher = lookup_barcode(number)[2]
+
+    result = {
+        "barcode": int(number),
+        "title": title,
+        "price": int(price),
+        "publisher": publisher
+    }
+
+    return result
