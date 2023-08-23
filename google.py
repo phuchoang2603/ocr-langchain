0e074e1216a26de8cfa3b89d50b2c0e3e8d36f1c
timz06 <91061595+phuchoang2603@users.noreply.github.com>
2023-08-24 00:02:02 +0700

    attempted to get the urls but cannot summarize it

--- google.py
+++ google.py
@@ -1,6 +1,14 @@
 import requests
 import urllib.parse
+from bs4 import BeautifulSoup
 import json
+from langchain.agents import initialize_agent, Tool
+from langchain.tools import BaseTool
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import tool
+from langchain.document_loaders import AsyncHtmlLoader
+from langchain.document_transformers import BeautifulSoupTransformer
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
 import os
 
@@ -9,29 +17,62 @@ load_dotenv()
 GOOGLE_API = os.getenv("GOOGLE_API")
 GOOGLE_SEARCH_ID = os.getenv("GOOGLE_SEARCH_ID")
 
-def search_product (title):
-    q = "phân biệt hàng giả hàng thật " + title
-
+@tool
+def search_product_links (query: str) -> str:
+    """Tìm kiếm các link giúp hỗ trợ phân biệt hàng thật hàng giả của một sản phẩm"""
+    
     data = {
     "key": GOOGLE_API,
     "cx": GOOGLE_SEARCH_ID,
-    "q": q
+    "q": query
     }
 
     # Convert data to a query string and encode it
     query_string = urllib.parse.urlencode(data)
     url = "https://www.googleapis.com/customsearch/v1?" + query_string
 
-    # Make the HTTP request
+    # Make the HTTP request and get five url to provide the model
     response = requests.get(url)
-    data = response.json()["items"]
+    data = response.json()["items"][:5]
 
-    for result in data:
-        process_individual_result(result)
+    # Return list of urls in json format
+    output = []
+    for item in data:
+        title = item["title"]
+        link = item["link"]
+        output.append({
+            "title": title,
+            "link": link
+        })
 
-def process_individual_result (result):
-    link = result["link"]
+    return json.dumps(output)
 
-    print(link)
 
-search_product("iphone 7")
+@tool
+def extract_info_from_tutorial(urls_array: str) -> str:
+    """Trích xuất thông tin từ các link hướng dẫn phân biệt hàng thật hàng giả của một sản phẩm
+    
+    Đầu vào của tool này là một mảng các link hướng dẫn đó"""
+
+    # Load HTML from urls
+    loader = AsyncHtmlLoader(urls_array)
+    htmls = loader.load()
+
+    # Transform HTML to BeautifulSoup
+    bs_transformer = BeautifulSoupTransformer()
+    docs_transformed = bs_transformer.transform_documents(htmls, tags_to_extract=["article"])
+
+    print("Extracting content with LLM")
+
+    # Grab the first 1000 tokens of the site
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=1000, 
+                                                                    chunk_overlap=0)
+    splits = splitter.split_documents(docs_transformed)
+
+
+llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0)
+tools = [search_product_links, extract_info_from_tutorial]
+agent = initialize_agent(tools, llm, agent='chat-zero-shot-react-description', verbose=True)
+
+result = agent.run("làm thế nào để phân biệt hàng thật hàng giả của iphone 7, trả lời theo ngôn ngữ tiếng việt")
+print(result)
